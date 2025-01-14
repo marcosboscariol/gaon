ee478a6f77c34caabd7508c055d93b60b22b8c75
Marcos Boscariol <118377554+marcosboscariol@users.noreply.github.com>
2025-01-14 12:14:19 -0300

    add file

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-
-from dotenv import load_dotenv
-import pandas as pd
-from crewai import Agent, Task, Crew
-from langchain_cohere import ChatCohere
-from crewai_tools import FileReadTool
-import os
 import streamlit as st
+import os
+from crewai_tools import FileReadTool
+from langchain_cohere import ChatCohere
+from crewai import Agent, Task, Crew
+import pandas as pd
+from dotenv import load_dotenv
+import sys
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 
 # Configuração de variáveis de ambiente
